6ecf24cdddcf5d82626840a319714c920c50adae
EyesWithoutFace <zaxorino@gmail.com>
2022-11-29 23:04:04 -0700

    barebones of app

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Browse reddit on any modern browser/device
 # Technologies used
 This app uses react, redux, html, css and javascript.
 Also uses git for version control
+
+## To-do
+* fetch reddit feed api
+* ensure app is usable on several browsers
+* write tests for the app
+* make app acessible with url w/github pages
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Main from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const e = React.createElement
+
+const domContainer = document.querySelector('#Main');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(Main));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
